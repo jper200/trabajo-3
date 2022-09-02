e92b4adc3ed93da87ce60a22f34973c38c6cbfa0
jper200 <112582736+jper200@users.noreply.github.com>
2022-09-02 13:09:53 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    n1 = n1 + 1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("" + (palabra1))
+})
+let n1 = 0
+let palabra1 = 0
+palabra1 = 0
+n1 = 0
