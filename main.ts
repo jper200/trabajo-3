ea1e00a5305bc8959cb426322adbcc3decd53991
jper200 <112582736+jper200@users.noreply.github.com>
2022-09-07 12:34:48 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0)
-    n1 = n1 + 1
+    basic.showNumber(n1)
+    n1 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    palabra1 = "bye"
+})
+input.onButtonPressed(Button.B, function () {
+    n1 += -1
+    basic.showNumber(n1)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("" + (palabra1))
+    basic.showString(palabra1)
 })
 let n1 = 0
-let palabra1 = 0
-palabra1 = 0
-n1 = 0
+let palabra1 = ""
+palabra1 = "hola"
+n1 = 15
